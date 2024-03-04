40b06287642cd02de5fd6d63a8f3055d57c20fff
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-04 13:52:04 +0100

    Setup wireless workflow on esp

--- Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
+++ Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
@@ -1,51 +1,71 @@
-#include <Arduino.h>
-#include "components/Component.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-Component *component;
+const char* ssid = "Antonelli";
+const char* password = "b8R8!(z534NK}o~";
+const char* mqtt_server = "192.168.1.53";
 
-const int F = 0.5;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-TaskHandle_t Task1;
-TaskHandle_t Task2;
+void setup_wifi() {
+  delay(5000);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-const int led_1 = 4;
-const int led_2 = 5;
+  WiFi.begin(ssid, password);
 
-void setup() {
-  component = new Component();
-  Serial.begin(115200); 
-  pinMode(led_1, OUTPUT);
-  pinMode(led_2, OUTPUT);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-  xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,1,&Task1,0);                         
-  delay(500); 
-
-  xTaskCreatePinnedToCore(Task2code,"Task2",10000,NULL,1,&Task2,1);          
-    delay(500); 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(100000);
 }
 
-
-void Task1code( void * parameter ){
-  for(;;){
-    Serial.print("Task1 is running on core ");
-    Serial.println(xPortGetCoreID());
-    digitalWrite(led_1, HIGH);
-    delay(500);
-    digitalWrite(led_1, LOW);
-    delay(500);
-  } 
-}
-
-void Task2code( void * parameter ){
-  for(;;){
-    Serial.print("Task2 is running on core ");
-    Serial.println(xPortGetCoreID());
-    digitalWrite(led_2, HIGH);
-    delay(1000);
-    digitalWrite(led_2, LOW);
-    delay(1000);
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, subscribe to desired topics
+      client.subscribe("esp8266/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
 }
 
-void loop() {}
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
 
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
