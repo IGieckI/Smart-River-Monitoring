f22be3d8161950ca27a0017cfefb64fea55ec238
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-06 00:16:56 +0100

    Add functions to check the connection with wifi and server, add also the sendToServer function

--- Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.cpp
+++ Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.cpp
@@ -23,3 +23,54 @@ int WaterLevelMonitor::getWaterLevel() {
         return distance;
     }
 }
+
+void WaterLevelMonitor::wifiSetup(const char *SSID, const char *password) {
+    Serial.print("Connecting to ");
+	Serial.println(WIFI_SSID);
+
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+}
+
+void WaterLevelMonitor::serverSetup(const char *domain, int port) {
+    client = PubSubClient(espClient);
+    client.setServer(domain, port);
+}
+
+void WaterLevelMonitor::reconnect() {
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+
+		if (client.connect("ESP8266Client")) {
+			Serial.println("connected");
+			client.subscribe(MQTT_TOPIC);
+		} else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			delay(5000);
+		}
+	}
+}
+
+bool WaterLevelMonitor::isConnectedToWifi() {
+    return WiFi.status() == WL_CONNECTED;
+}
+
+bool WaterLevelMonitor::isConnectedToServer() {
+    client.loop();
+    return client.connected();
+}
+
+void WaterLevelMonitor::sendToServer(const char *topic, const char *message) {
+    client.publish(topic, message);
+}
--- Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.h
+++ Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.h
@@ -1,6 +1,8 @@
 #ifndef __WATERLEVELMONITOR__
 #define __WATERLEVELMONITOR__
 
+#include <WiFi.h>
+#include <PubSubClient.h>
 #include "Config.h"
 #include "components/distantiometer/Sonar.h"
 #include "components/light/Led.h"
@@ -10,14 +12,21 @@ class WaterLevelMonitor {
         Led *greenLed;
         Led *redLed;
         DistanceSensor *distanceSensor;
-        
-        int carWashed;
+        WiFiClient espClient;
+        PubSubClient client;
+
+        void reconnect();
     
     public:
         WaterLevelMonitor();
         Led *getGreenLed();
         Led *getRedLed();
         int getWaterLevel();
+        bool isConnectedToWifi();
+        bool isConnectedToServer();
+        void wifiSetup(const char *SSID, const char *password);
+        void serverSetup(const char *domain, int port);
+        void sendToServer(const char *topic, const char *message);
 };
 
 #endif
