d390aba697fc2e1f64da9f803a6f7c027c7e77cb
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-05 20:40:37 +0100

    MQTT setup and data read on main loop

--- Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
+++ Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
@@ -1,87 +1,72 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <Config.h>
+#include "model/WaterLevelMonitor.h"
 
 TaskHandle_t communication_task, data_task;
-
-const char* ssid = "Antonelli";
-const char* password = "b8R8!(z534NK}o~";
-const char* mqtt_server = "192.168.1.53";
-const int millis_data_frequency = 500;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup_wifi() {
-  delay(5000);
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+WaterLevelMonitor model;
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  delay(100000);
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-      // Once connected, subscribe to desired topics
-      client.subscribe("esp8266/output");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
+int cont = 0;
 
 void setup() {
   Serial.begin(115200);
+
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+
+  client.setServer(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  	client.loop();
+	if (!client.connected()) {
+		model.getRedLed()->switchLight(true);
+	  	model.getGreenLed()->switchLight(false);
+		reconnect();
+		model.getGreenLed()->switchLight(true);
+	  	model.getRedLed()->switchLight(false);
+  	}
+
+	short waterLevel = cont++;//model.getWaterLevel();
+
+	client.publish(MQTT_TOPIC, String(waterLevel).c_str());
+
+	delay(UPDATE_FREQUENCY);
 }
 
-void communication_task_fun( void * parameter ){
-  for(;;){
+void setup_wifi() {
+	delay(5000);
+	Serial.print("Connecting to ");
+	Serial.println(WIFI_SSID);
 
-  }
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
-void data_task_fun( void * parameter ){
-  for(;;){
-    
-    delay(millis_data_frequency);
-  }
+void reconnect() {
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+
+		if (client.connect("ESP8266Client")) {
+			Serial.println("connected");
+			client.subscribe(MQTT_TOPIC);
+		} else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			delay(5000);
+		}
+	}
 }
