e1b2032aeeaabfed6bbc7ac1f82fcc7c843b51ce
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-06 00:17:19 +0100

    Refactory of main loop with the new model

--- Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
+++ Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
@@ -1,72 +1,31 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
+
 #include <Config.h>
 #include "model/WaterLevelMonitor.h"
 
-TaskHandle_t communication_task, data_task;
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
 WaterLevelMonitor model;
 
-int cont = 0;
-
 void setup() {
-  Serial.begin(115200);
+  	Serial.begin(115200);
+	
+	model.getRedLed()->switchLight(true);
+	model.getGreenLed()->switchLight(false);
 
-  setup_wifi();
-
-  client.setServer(MQTT_SERVER, MQTT_PORT);
+  	model.wifiSetup(WIFI_SSID, WIFI_PASSWORD);
+  	model.serverSetup(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
-  	client.loop();
-	if (!client.connected()) {
+	if (!(model.isConnectedToServer() && model.isConnectedToWifi())) {
 		model.getRedLed()->switchLight(true);
 	  	model.getGreenLed()->switchLight(false);
-		reconnect();
+  	} else {
 		model.getGreenLed()->switchLight(true);
 	  	model.getRedLed()->switchLight(false);
-  	}
+	}
 
-	short waterLevel = cont++;//model.getWaterLevel();
+	short waterLevel = model.getWaterLevel();
 
-	client.publish(MQTT_TOPIC, String(waterLevel).c_str());
+	model.sendToServer(MQTT_TOPIC, String(waterLevel).c_str());
 
 	delay(UPDATE_FREQUENCY);
-}
-
-void setup_wifi() {
-	delay(5000);
-	Serial.print("Connecting to ");
-	Serial.println(WIFI_SSID);
-
-	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
-	
-	Serial.println("");
-	Serial.println("WiFi connected");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
-}
-
-void reconnect() {
-	while (!client.connected()) {
-		Serial.print("Attempting MQTT connection...");
-
-		if (client.connect("ESP8266Client")) {
-			Serial.println("connected");
-			client.subscribe(MQTT_TOPIC);
-		} else {
-			Serial.print("failed, rc=");
-			Serial.print(client.state());
-			Serial.println(" try again in 5 seconds");
-			delay(5000);
-		}
-	}
 }
