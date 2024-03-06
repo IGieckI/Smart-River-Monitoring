edd21bbafc55407885f7fd11c44b4147c2b3659c
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-07 00:34:32 +0100

    Add ethernet support

--- Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.h
+++ Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.h
@@ -2,18 +2,25 @@
 #define __WATERLEVELMONITOR__
 
 #include <WiFi.h>
+#include <Ethernet.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include "Config.h"
 #include "components/distantiometer/Sonar.h"
 #include "components/light/Led.h"
+
+enum ConnectionType { NONE, WIFI, ETHERNET };
 
 class WaterLevelMonitor {
     private:
         Led *greenLed;
         Led *redLed;
         DistanceSensor *distanceSensor;
-        WiFiClient espClient;
+        WiFiClient wifiClient;
+        EthernetClient ethClient;
         PubSubClient client;
+
+        ConnectionType connectionType;
 
         void reconnect();
     
@@ -25,8 +32,9 @@ class WaterLevelMonitor {
         bool isConnectedToWifi();
         bool isConnectedToServer();
         void wifiSetup(const char *SSID, const char *password);
+        void ethernetSetup(const uint8_t *mac, const int *ip);
         void serverSetup(const char *domain, int port);
-        void sendToServer(const char *topic, const char *message);
+        void sendToServer(const char *topic, int waterLevel);
 };
 
 #endif
