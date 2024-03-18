0d6c33fb8e3b9f1cb320bb0ab5605b26b369c41f
IGieckI <giacomo.antonelli3@gmail.com>
2024-03-18 09:59:39 +0100

    Move connection related things into main

--- Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
+++ Water_Level_Monitor_SubSys/src/Water_Level_Monitor_SubSys.ino
@@ -1,8 +1,13 @@
 
 #include <Config.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include "model/WaterLevelMonitor.h"
 
 WaterLevelMonitor model(RED_LED_PIN, GREEN_LED_PIN, DISTANCE_SENSOR_ECHO_PIN, DISTANCE_SENSOR_TRIG_PIN, MAX_DISTANCE_TIME);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 void setup() {
 	delay(1000);
@@ -11,13 +16,12 @@ void setup() {
 	model.switchRedLed(true);
 	model.switchGreenLed(false);
 
-  	model.wifiSetup(WIFI_SSID, WIFI_PASSWORD);
-	//model.ethernetSetup(MAC, IP);
-  	model.serverSetup(MQTT_SERVER, MQTT_PORT);
+	wifiSetup(WIFI_SSID, WIFI_PASSWORD);
+  	serverSetup(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
-	if (model.isConnectedToServer() && model.isConnectedToWifi()) {
+	if (isConnectedToServer() && isConnectedToWifi()) {
 		model.switchGreenLed(true);
 	  	model.switchRedLed(false);
   	} else {
@@ -30,7 +34,80 @@ void loop() {
 	Serial.print("Water level: ");
 	Serial.println(waterLevel);
 
-	model.sendToServer(MQTT_TOPIC, waterLevel);
+	sendToServer(MQTT_TOPIC, waterLevel);
 
 	delay(UPDATE_FREQUENCY);
+}
+
+void wifiSetup(const char *SSID, const char *password) {
+    Serial.print("Connecting to ");
+	Serial.println(SSID);
+
+	WiFi.begin(SSID, password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+}
+
+void serverSetup(const char *domain, int port) {
+	client = PubSubClient();
+	
+	Serial.println("---------------------");
+	Serial.println("Server setup\n");
+	Serial.println(domain);
+	Serial.println(port);
+
+	Serial.println("---------------------");
+	delay(100);
+    client.setServer(domain, port);
+	while(!client.connect("ESP8266Client")) {
+		Serial.println("Emmh");
+		Serial.println(isConnectedToWifi() ? "WiFi connected" : "WiFi not connected");
+		Serial.print("Server connection failed, state: ");
+		Serial.println(client.state());
+		delay(500);
+	}
+
+	Serial.println("Nice");
+}
+
+void reconnect() {
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+
+		if (client.connect("ESP8266Client")) {
+			Serial.println("connected");
+		} else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			delay(5000);
+		}
+	}
+}
+
+void sendToServer(const char *topic, int waterLevel) {
+	JsonDocument doc;
+	doc["water_level"] = waterLevel;
+	char mqtt_message[128];
+	serializeJson(doc, mqtt_message);
+
+    Serial.println(client.publish(topic, mqtt_message) ? "Message sent" : "Message failed");
+	Serial.println(client.state());
+}
+
+bool isConnectedToWifi() {
+    return WiFi.status() == WL_CONNECTED;
+}
+
+bool isConnectedToServer() {
+    client.loop();
+    return client.connected();
 }
--- Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.h
+++ Water_Level_Monitor_SubSys/src/model/WaterLevelMonitor.h
@@ -1,10 +1,6 @@
 #ifndef __WATERLEVELMONITOR__
 #define __WATERLEVELMONITOR__
 
-#include <WiFi.h>
-#include <Ethernet.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
 #include "components/distantiometer/Sonar.h"
 #include "components/light/Led.h"
 
@@ -13,22 +9,12 @@ class WaterLevelMonitor {
         Led *greenLed;
         Led *redLed;
         DistanceSensor *distanceSensor;
-        WiFiClient wifiClient;
-        PubSubClient client;
-
-        void reconnect();
     
     public:
         WaterLevelMonitor(uint8_t redLedPin, uint8_t greenLedPin, uint8_t echoPin, uint8_t trigPin, int maxDistanceTime);
         void switchRedLed(bool state);
         void switchGreenLed(bool state);
         int getWaterLevel(int minDistance);
-        bool isConnectedToWifi();
-        bool isConnectedToServer();
-        void wifiSetup(const char *SSID, const char *password);
-        void ethernetSetup(const uint8_t *mac, const int *ip);
-        void serverSetup(const char *domain, int port);
-        void sendToServer(const char *topic, int waterLevel);
 };
 
 #endif
