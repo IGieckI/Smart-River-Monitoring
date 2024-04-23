8227ebe4dbb06467d83bc9b6f2e5ed4f275ba779
IGieckI <giacomo.antonelli3@gmail.com>
2024-04-23 21:21:24 +0200

    Implementation of a dynamic configuration protocol (ESP side)

--- Water_Level_Monitor_SubSys/src/Config.h
+++ Water_Level_Monitor_SubSys/src/Config.h
@@ -9,19 +9,17 @@
 
 // Distance sensor settings
 #define MAX_DISTANCE_TIME 1000000l // microsecond time limit to wait for a distance sensor range
-#define MIN_DISTANCE 6
 #define MAX_DISTANCE 26
 #define MAX_ERROR_DISTANCE 30 // If distance > MAX_ERROR_DISTANCE, then the distance sensor is not reading correctly
 
 // Other Constants
 #define BAUD_RATE 115200
-#define UPDATE_FREQUENCY 1000 // millisecond update frequency time
 
 #define WIFI_SSID "Focantenna"//"SmartRiverMonitoring"
 #define WIFI_PASSWORD "zprv9600"//"password123"
 
 #define MQTT_SERVER "broker.mqtt-dashboard.com"
 #define MQTT_PORT 1883
-#define MQTT_TOPIC "water_level_iot_24h"
+#define MQTT_INITIALIZATION_TOPIC "water_level_iot_24h/protocol"
 
 #endif
